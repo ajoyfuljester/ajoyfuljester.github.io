ef0359f5c9057b87e04ddab9aab402c71e324b66
ajoyfuljester <ajoyfuljester@protonmail.com>
2022-08-31 22:01:27 +0200

    added something2 with a jumpscare

--- js/something.js
+++ js/something.js
@@ -1,5 +1,16 @@
 document.querySelectorAll('body > div > div').forEach((element) => {
     element.onclick = () => {
-        element.classList.toggle('flexFullScreen')
+        element.classList.toggle('fullscreen')
+        // JUMPSCARE
+        let jumpscare = document.createElement('div')
+        jumpscare.classList.add('jumpscare')
+        document.body.appendChild(jumpscare)
+        jumpscare.requestFullscreen()
+        let sound = new Audio('/jumpscare.mp3')
+        sound.play()
+        
+        setTimeout(() => {
+            jumpscare.remove()
+        }, 3000)
     }
 })
