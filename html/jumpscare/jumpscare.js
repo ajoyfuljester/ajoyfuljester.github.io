c28d561337dfe75aa56c50fb19362d2bce9f0057
ajoyfuljester <ajoyfuljester@protonmail.com>
2022-09-08 17:54:45 +0200

    creted menu with iframe, made small adjustments to the ??? site

--- html/jumpscare/jumpscare.js
+++ html/jumpscare/jumpscare.js
@@ -1,14 +1,19 @@
 function jumpscare() {
     let jumpscare = document.createElement('div')
+    jumpscare.onfullscreenchange = () => {
+        if (document.fullscreenElement === null) {
+            jumpscare.remove()
+        }
+    }
+
     jumpscare.classList.add('jumpscare')
     document.body.appendChild(jumpscare)
     jumpscare.requestFullscreen()
     let sound = new Audio('/html/jumpscare/jumpscare.mp3')
     sound.play();
     
-    setTimeout(async () => {
-        await document.exitFullscreen()
-        jumpscare.remove()
+    setTimeout(() => {
+        document.exitFullscreen()
     }, 3000)
     
 }
